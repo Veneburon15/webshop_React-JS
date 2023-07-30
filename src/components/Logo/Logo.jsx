9f90365ca6e3d567465093b6559619049e3b4425
Angel Leon <angel.leon.ram@gmail.com>
2023-07-30 13:28:52 -0300

    Wbshop final, corrrección de errores de base de datos

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <>
+            <Link className='navigation' to="/">
+                <img className="img-fluid nav__logo" src="..\images\wpp.jpg" alt="Logo Èclat CoffeShop" />
+            </Link>
+        </>
+    )
+}
+
+export default Logo
